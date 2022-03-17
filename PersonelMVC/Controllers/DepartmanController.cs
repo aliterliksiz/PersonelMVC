ad4353ec071c6aaab10f359d5a249b42b867a4e5
aliterliksiz <aliterliksiz46@gmail.com>
2022-03-17 16:43:23 +0300

    Add project files.

---
+++ PersonelMVC/Controllers/DepartmanController.cs
@@ -0,0 +1,23 @@
+﻿using PersonelMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonelMVC.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        public ActionResult Merhaba()
+        {
+            var departman = new Departman() { Id = 5, Ad = "Bilgi işlem" };
+            return View(departman);  // View --> Tarayıcıya HTML çıktı dönderen bir Action Result tipidir.
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
